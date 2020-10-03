6a485693ef21980519f5e8c439030ef45eeb5005
Parth Pandey <parthpandey2236@gmail.com>
2020-10-04 03:14:56 +0530

    Added switch

--- lib/AboutPage.dart
+++ lib/AboutPage.dart
@@ -2,6 +2,7 @@ import 'package:elchackathon_app/main.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:custom_switch/custom_switch.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -15,103 +16,168 @@ class About extends StatefulWidget {
 }
 
 class _AboutState extends State<About> {
+  String language="हिंदी";
+   bool status=false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.only(bottom: 150.0),
-          child: Column(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(
-                    left: MediaQuery.of(context).size.width - 80,
-                    top: 10,
-                    right: 20),
-                child: FlatButton(
-                  onPressed: () async {
-                    //SharedPreferences prefs = await SharedPreferences.getInstance();
-                    SharedPreferences prefs =
-                        await SharedPreferences.getInstance();
-                    prefs.setString('email', null);
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => Login()));
-                  },
-                  child: Icon(
-                    EvaIcons.logOutOutline,
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.only(bottom: 150.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                children:[
+                  CustomSwitch(
+
+                    activeColor: Colors.pink,
+                    value: status,
+                    onChanged: (value) {
+                      status = value;
+                      if(status=="true"){
+                        language="English";
+                      }
+                      else{
+                        language="हिंदी";
+                      }
+                    },
+
                   ),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                      side: BorderSide(color: Colors.black)),
+                  Text(
+                    language,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                Container(
+                  margin: EdgeInsets.only(
+                      left: MediaQuery.of(context).size.width -90,
+
+                      top: 10,
+                      right: 20),
+                  child: FlatButton(
+                    onPressed: () async {
+                      //SharedPreferences prefs = await SharedPreferences.getInstance();
+                      SharedPreferences prefs =
+                          await SharedPreferences.getInstance();
+                      prefs.setString('email', null);
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Login()));
+                    },
+                    child: Icon(
+                      EvaIcons.logOutOutline,
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                        side: BorderSide(color: Colors.black)),
+                  ),
                 ),
-              ),
-              Container(
-                color: Color(0xff667aff),
-                child: Text(
-                  'About the App',
-                  style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40.0,
+
+                  Text(
+                    language,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  )
+                ]
+                ),
+                Container(
+                  color: Color(0xff667aff),
+                  child: Text(
+                    'About the App',
+                    style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontSize: 40.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 150.0,
+                  height: 20.0,
+                  child: Divider(
                     color: Colors.black,
-                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              SizedBox(
-                width: 150.0,
-                height: 20.0,
-                child: Divider(
-                  color: Colors.black,
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Text(
+                    'Healthx is an app built to act as a virtual bridge between the suffering and towards the goal of getting cured. It was built with the vision in mind to reduce the hassle of the suffering of connecting to the doctors and also understanding what to do and what not to do in such times. Hope this application helps in any small way possible to reduce the pain of our friends worldwide who are suffering. This application is focused on serving the people who has a high chance of having breast cancer/already suffering patients and it strives to make their lives a little bit better.',
+                    style: TextStyle(
+                      fontFamily: 'SansitaSwashed',
+                      fontSize: 20.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Text(
-                  'Healthx is an app built to act as a virtual bridge between the suffering and towards the goal of getting cured. It was built with the vision in mind to reduce the hassle of the suffering of connecting to the doctors and also understanding what to do and what not to do in such times. Hope this application helps in any small way possible to reduce the pain of our friends worldwide who are suffering. This application is focused on serving the people who has a high chance of having breast cancer/already suffering patients and it strives to make their lives a little bit better.',
-                  style: TextStyle(
-                    fontFamily: 'SansitaSwashed',
-                    fontSize: 20.0,
+                Container(
+                  color: Color(0xff667aff),
+                  child: Text(
+                    'Developers',
+                    style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontSize: 40.0,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 150.0,
+                  height: 20.0,
+                  child: Divider(
                     color: Colors.black,
-                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              Container(
-                color: Color(0xff667aff),
-                child: Text(
-                  'Developers',
-                  style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40.0,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              SizedBox(
-                width: 150.0,
-                height: 20.0,
-                child: Divider(
-                  color: Colors.black,
-                ),
-              ),
-              Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(35.0),
-                    child: Column(
+                Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(35.0),
+                      child: Column(
+                        children: <Widget>[
+                          CircleAvatar(
+                            radius: 50.0,
+                            backgroundImage: AssetImage('assets/Eeshan (3).jpg'),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Container(
+                              color: Color(0xff667aff),
+                              child: Text(
+                                'Eeshan Dutta',
+                                style: TextStyle(
+                                  fontFamily: 'Pacifico',
+                                  fontSize: 20.0,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Column(
                       children: <Widget>[
                         CircleAvatar(
                           radius: 50.0,
-                          backgroundImage: AssetImage('assets/Eeshan (3).jpg'),
+                          backgroundImage: AssetImage('assets/Soham.jpg'),
                         ),
                         Padding(
                           padding: const EdgeInsets.all(10.0),
                           child: Container(
                             color: Color(0xff667aff),
                             child: Text(
-                              'Eeshan Dutta',
+                              'Soham Sakaria',
                               style: TextStyle(
                                 fontFamily: 'Pacifico',
                                 fontSize: 20.0,
@@ -122,51 +188,52 @@ class _AboutState extends State<About> {
                         ),
                       ],
                     ),
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Column(
-                    children: <Widget>[
-                      CircleAvatar(
-                        radius: 50.0,
-                        backgroundImage: AssetImage('assets/Soham.jpg'),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Container(
-                          color: Color(0xff667aff),
-                          child: Text(
-                            'Soham Sakaria',
-                            style: TextStyle(
-                              fontFamily: 'Pacifico',
-                              fontSize: 20.0,
-                              color: Colors.black,
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(35.0),
+                      child: Column(
+                        children: <Widget>[
+                          CircleAvatar(
+                            radius: 50.0,
+                            backgroundImage:
+                                AssetImage('assets/parth pandey.jpg'),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Container(
+                              color: Color(0xff667aff),
+                              child: Text(
+                                'Parth Pandey',
+                                style: TextStyle(
+                                  fontFamily: 'Pacifico',
+                                  fontSize: 20.0,
+                                  color: Colors.black,
+                                ),
+                              ),
                             ),
                           ),
-                        ),
+                        ],
                       ),
-                    ],
-                  ),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(35.0),
-                    child: Column(
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Column(
                       children: <Widget>[
                         CircleAvatar(
                           radius: 50.0,
                           backgroundImage:
-                              AssetImage('assets/parth pandey.jpg'),
+                              AssetImage('assets/Parth Srivastava.jpeg'),
                         ),
                         Padding(
                           padding: const EdgeInsets.all(10.0),
                           child: Container(
                             color: Color(0xff667aff),
                             child: Text(
-                              'Parth Pandey',
+                              'Parth Srivastava',
                               style: TextStyle(
                                 fontFamily: 'Pacifico',
                                 fontSize: 20.0,
@@ -177,36 +244,10 @@ class _AboutState extends State<About> {
                         ),
                       ],
                     ),
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Column(
-                    children: <Widget>[
-                      CircleAvatar(
-                        radius: 50.0,
-                        backgroundImage:
-                            AssetImage('assets/Parth Srivastava.jpeg'),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Container(
-                          color: Color(0xff667aff),
-                          child: Text(
-                            'Parth Srivastava',
-                            style: TextStyle(
-                              fontFamily: 'Pacifico',
-                              fontSize: 20.0,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              )
-            ],
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
